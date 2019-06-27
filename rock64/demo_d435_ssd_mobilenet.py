2b894d7cc8dcb09b5b997731d9b1745ab8054570
kenji ogura <k5i.ogura.v40@gmail.com>
2019-06-27 18:43:44 +0000

    revise

--- rock64/demo_d435_ssd_mobilenet.py
+++ rock64/demo_d435_ssd_mobilenet.py
@@ -2,7 +2,7 @@
 import argparse
 import platform
 import subprocess
-from edgetpu.detection.engine import DetectionEngine
+from multiprocessing import Process, Queue, Value
 import pyrealsense2 as rs
 from PIL import Image
 from PIL import ImageDraw
@@ -14,118 +14,61 @@ from time import time, sleep
 from pdb import *
 
 class D435:
-    def __init__(self, color=True, depth=False, w=640, h=480, fps=30):
-        self.color    = color
-        self.depth    = depth
-        self.pipeline = rs.pipeline()
-        config        = rs.config()
-        self.align_to = rs.stream.color
-        self.align    = rs.align(self.align_to)
-        if color: config.enable_stream(rs.stream.color, w, h, rs.format.bgr8, fps)
-        if depth: config.enable_stream(rs.stream.depth, w, h, rs.format.z16,  fps)
-        profile = self.pipeline.start(config)
-        self.scale    = profile.get_device().first_depth_sensor().get_depth_scale()
-        print("depth_sensor_scale:",self.scale)
+      def __init__(self, color=True, depth=False, w=640, h=480, fps=30):
+          self.color    = color
+          self.depth    = depth
+          self.pipeline = rs.pipeline()
+          config        = rs.config()
+          self.align_to = rs.stream.color
+          self.align    = rs.align(self.align_to)
+          if color: config.enable_stream(rs.stream.color, w, h, rs.format.bgr8, fps)
+          if depth: config.enable_stream(rs.stream.depth, w, h, rs.format.z16,  fps)
+          profile = self.pipeline.start(config)
+          self.scale    = profile.get_device().first_depth_sensor().get_depth_scale()
+          print("depth_sensor_scale:",self.scale)
 
-    def read(self):
-        color_frame = depth_frame = None
-        frame = self.pipeline.wait_for_frames()
-        align_frame = self.align.process(frame)
-        if self.color:
-            color_frame = align_frame.get_color_frame()
-            color_frame = np.asanyarray(color_frame.get_data())
-        if self.depth:
-            depth_frame = align_frame.get_depth_frame()
-            depth_frame1= depth_frame.as_depth_frame()
-            depth_array = np.asanyarray(depth_frame.get_data())
-        return color_frame, depth_frame, depth_array
+      def read(self):
+          color_frame = depth_frame = depth_array = None
+          frame = self.pipeline.wait_for_frames()
+          align_frame = self.align.process(frame)
+          if self.color:
+              color_frame = align_frame.get_color_frame()
+              color_frame = np.asanyarray(color_frame.get_data())
+          if self.depth:
+              depth_frame = align_frame.get_depth_frame()
+              depth_frame1= depth_frame.as_depth_frame()
+              depth_array = np.asanyarray(depth_frame.get_data())
+          return color_frame, depth_frame, depth_array
 
-    def release(self):
-        sleep(1)
-        self.pipeline.stop()
-        sleep(1)
+      def release(self):
+          self.pipeline.stop()
 
 # Function to read labels from text files.
 def ReadLabelFile(file_path):
-  with open(file_path, 'r', encoding="utf-8") as f:
-    lines = f.readlines()
-  ret = {}
-  for line in lines:
-    pair = line.strip().split(maxsplit=1)
-    ret[int(pair[0])] = pair[1].strip()
-  return ret
+    with open(file_path, 'r', encoding="utf-8") as f:
+      lines = f.readlines()
+    ret = {}
+    for line in lines:
+      pair = line.strip().split(maxsplit=1)
+      ret[int(pair[0])] = pair[1].strip()
+    return ret
 
+def inf_proc(qa, qb, args, status):
+    from edgetpu.detection.engine import DetectionEngine
+    engine = DetectionEngine(args.model)
+    while True:
+        img = qa.get()
+        if status.value != 0:break
+        ans = engine.DetectWithImage(img, threshold=args.threshold, keep_aspect_ratio=True,
+                                     relative_coord=False, top_k=10)
+        if qb.full(): qb.get()
+        qb.put(ans)
 
-def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '-m', '--model', type=str,
-      default='../model/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite',
-      help='Path of the detection model.'
-  )
-  parser.add_argument(
-      '-l', '--label',type=str,
-      default='../model/coco_labels.txt',
-      help='Path of the labels file.'
-  )
-  parser.add_argument(
-      '--input', help='File path of the input image.')
-  parser.add_argument(
-      '--output', help='File path of the output image.')
-  parser.add_argument(
-      '--threshold', type=float, default=0.45, help='Threshold for DetectionEngine')
-  parser.add_argument( '-d',   '--depth',    action='store_true')
-  parser.add_argument( '-r',   '--rect',     action='store_true')
-  parser.add_argument( '-uvc', '--uvc',      action='store_true')
-  parser.add_argument( '-center', '--object_center', action='store_true')
-  parser.add_argument( '-crw', '--NN_w',     type=int, default=320, help='NeuralNet in-width size')
-  parser.add_argument( '-crh', '--NN_h',     type=int, default=240, help='NeuralNet in-height size')
-  parser.add_argument( '-rww', '--resize_w', type=int, default=640, help='resize view windows width')
-  parser.add_argument( '-rwh', '--resize_h', type=int, default=480, help='resize view windows height')
-  args = parser.parse_args()
-
-  engine = DetectionEngine(args.model)
-  labels = ReadLabelFile(args.label) if args.label else None
-
-  print("NN: {} {} | uvc: {} | depth : {}".format(args.NN_w, args.NN_h, args.uvc, args.depth))
-  if args.uvc:
-      cam = cv2.VideoCapture(0)
-      assert cam is not None
-      print("Opened UVC-Camera via /dev/video0")
-      cam.set(cv2.CAP_PROP_FPS,30)
-      cam.set(cv2.CAP_PROP_FRAME_WIDTH,args.NN_w)
-      cam.set(cv2.CAP_PROP_FRAME_HEIGHT,args.NN_h)
-  else:
-      cam = D435(color=True, depth=args.depth)
-
-  start = None
-  img_count = 0
-  seg = None
-  ratio_w = args.resize_w/args.NN_w
-  ratio_h = args.resize_h/args.NN_h
-  while True:
-      if start is None: start = time()
-      cam_start = time()
-      if args.uvc:
-          r, Img_org = cam.read()
-          assert r is True
-          Img = cv2.cvtColor(Img_org, cv2.COLOR_BGR2RGB)
-      else:
-          Img_org, dth, dth_np = cam.read()
-          Img = cv2.resize(Img_org,(args.NN_w,args.NN_h))
-      if seg is None: seg = np.zeros(Img_org.shape,dtype=np.uint8)
-      img = Image.fromarray(np.uint8(Img))
-      #draw = ImageDraw.Draw(img)
-
-      # Run inference.
-      cam_time = time() - cam_start
-      inf_start = time()
-      ans = engine.DetectWithImage(img, threshold=args.threshold, keep_aspect_ratio=True,
-                                   relative_coord=False, top_k=10)
-      inf_time = time() - inf_start
-      drw_start= time()
-      img_count+=1
-      if ans:
+def drw_proc(qi, args, labels, cam_scale, ratio_w, ratio_h, status):
+    seg = None
+    while True:
+        ans, Img_org, dth_np = qi.get()
+        if seg is None: seg = np.zeros(Img_org.shape,dtype=np.uint8)
         seg = (seg/2).astype(np.uint8)
         for obj in ans:
             txt = labels[obj.label_id]
@@ -136,10 +79,10 @@ def main():
             rect_xy = (int(rect_lt[0]+(rect_rb[0] - rect_lt[0])/2),int(rect_lt[1]+(rect_rb[1] - rect_lt[1])/2))
             if args.depth:
                 if not args.object_center:
-                    dth_obj_m= dth_np[rect_lt[1]:rect_rb[1], rect_lt[0]:rect_rb[0]]*cam.scale
-                    dth_obj_m = np.clip(dth_obj_m, 0.001, 10.000)         # histogram of meter wise until 20m
+                    dth_obj_m= dth_np[rect_lt[1]:rect_rb[1], rect_lt[0]:rect_rb[0]]*cam_scale
+                    dth_obj_m = np.clip(dth_obj_m, 0.001, 10.000) # histogram of meter wise until 20m
                     bins, range_m = np.histogram(dth_obj_m, bins=10)
-                    index_floor = np.argmax(bins)                         # range which occupy most area in bbox
+                    index_floor = np.argmax(bins)                 # range which occupy most area in bbox
                     range_floor = range_m[index_floor]
                     range_ceil  = range_m[index_floor+1]
                     indexXY = np.where((dth_obj_m>range_floor) & (dth_obj_m<range_ceil))
@@ -153,23 +96,114 @@ def main():
                 txt    = txt + " %.2fm"%(meters)
             if args.rect: cv2.rectangle(Img_org, rect_lt, rect_rb, (255,255,255), 2)
             cv2.putText(Img_org, txt, rect_xy, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-      Img_org = Img_org | seg
-      cv2.imshow('demo',Img_org)
-      key=cv2.waitKey(1)
-      if key==27: break
-      during = (time()-start)
-      drw_time = time() - drw_start
-      sys.stdout.write('\b'*80)
-      sys.stdout.write(
-          "%.3fFPS cam:%.3f inf:%.3f drw:%.3f %dobjects"%
-          (img_count/during, cam_time, inf_time, drw_time, len(ans))
-      )
-      sys.stdout.flush()
-      continue
+        Img_org = Img_org | seg
+        cv2.imshow('demo',Img_org)
+        key=cv2.waitKey(1)
+        if key==27: break
+    status.value=1
 
-  print("\nfin")
-  cv2.destroyAllWindows()
-  cam.release()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-m', '--model', type=str,
+        default='../model/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite',
+        help='Path of the detection model.'
+    )
+    parser.add_argument(
+        '-l', '--label',type=str,
+        default='../model/coco_labels.txt',
+        help='Path of the labels file.'
+    )
+    parser.add_argument(
+        '--input', help='File path of the input image.')
+    parser.add_argument(
+        '--output', help='File path of the output image.')
+    parser.add_argument(
+        '--threshold', type=float, default=0.45, help='Threshold for DetectionEngine')
+    parser.add_argument( '-d',   '--depth',    action='store_true')
+    parser.add_argument( '-r',   '--rect',     action='store_true')
+    parser.add_argument( '-uvc', '--uvc',      action='store_true')
+    parser.add_argument( '-center', '--object_center', action='store_true')
+    parser.add_argument( '-crw', '--NN_w',     type=int, default=320, help='NeuralNet in-width size')
+    parser.add_argument( '-crh', '--NN_h',     type=int, default=240, help='NeuralNet in-height size')
+    parser.add_argument( '-rww', '--resize_w', type=int, default=640, help='resize view windows width')
+    parser.add_argument( '-rwh', '--resize_h', type=int, default=480, help='resize view windows height')
+    args = parser.parse_args()
+
+    print("NN: {} {} | uvc: {} | depth : {}".format(args.NN_w, args.NN_h, args.uvc, args.depth))
+    if args.uvc:
+        cam = cv2.VideoCapture(0)
+        assert cam is not None
+        print("Opened UVC-Camera via /dev/video0")
+        cam.set(cv2.CAP_PROP_FPS,30)
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH,args.NN_w)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT,args.NN_h)
+    else:
+        cam = D435(color=True, depth=args.depth)
+
+    labels = ReadLabelFile(args.label) if args.label else None
+    start = None
+    img_count = 0
+    ratio_w = args.resize_w/args.NN_w
+    ratio_h = args.resize_h/args.NN_h
+
+    drw_status = Value('i',0)
+    inf_status = Value('i',0)
+    qi = Queue(3)
+    qa = Queue(3)
+    qb = Queue(3)
+    drw_subp = Process(target=drw_proc, args=(qi, args, labels, cam.scale, ratio_w, ratio_h, drw_status,))
+    inf_subp = Process(target=inf_proc, args=(qa, qb, args, inf_status, ))
+    drw_subp.start()
+    inf_subp.start()
+    ans_latest = []
+    qa_dummy   = None
+    while True:
+        if start is None: start = time()
+        cam_start = time()
+        if args.uvc:
+            r, Img_org = cam.read()
+            assert r is True
+            Img = cv2.cvtColor(Img_org, cv2.COLOR_BGR2RGB)
+        else:
+            Img_org, dth, dth_np = cam.read()
+            Img = cv2.resize(Img_org,(args.NN_w,args.NN_h))
+        img = Image.fromarray(np.uint8(Img))
+        #draw = ImageDraw.Draw(img)
+
+        # Run inference.
+        cam_time = time() - cam_start
+        inf_start = time()
+        if qa.full(): qa.get()
+        qa_dummy = img
+        qa.put(img)
+        try:
+            ans_latest = qb.get_nowait()
+        except:
+            pass
+
+        inf_time = time() - inf_start
+        drw_start= time()
+        img_count+=1
+
+        if qi.full():qi.get()
+        qi.put([ans_latest, Img_org, dth_np])
+        if drw_status.value != 0: break
+
+        during = (time()-start)
+        drw_time = time() - drw_start
+        sys.stdout.write('\b'*80)
+        sys.stdout.write(
+            "%.3fFPS cam:%.3f inf:%.3f drw:%.3f %dobjects"%
+            (img_count/during, cam_time, inf_time, drw_time, len(ans_latest))
+        )
+        sys.stdout.flush()
+
+    print("\nfin")
+    qa.put(qa_dummy)
+    inf_status.value = 1
+    cam.release()
 
 if __name__ == '__main__':
-  main()
+    main()
